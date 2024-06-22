309d2ce13f736c84232ce24c74f2f11a07c4977d
gstill498463 <gabriel.m.still@gmail.com>
2024-06-22 13:46:30 -0700

    fixed js file not found and css file not found

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const fadeInSections = document.querySelectorAll(".fade-in-section");
-
+    console.log("domContentLoaded");
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
