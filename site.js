9037b67ef6d5d7c3898a11094ade1e861538714a
gstill498463 <gabriel.m.still@gmail.com>
2024-06-25 19:31:40 -0700

    consolelog mouse hover

--- site.js
+++ site.js
@@ -181,6 +181,7 @@ const handleOnMouseHover = e => {
         y = e.clientY - rect.top;
     target.style.setProperty("--mouse-x", `${x}px`);
     target.style.setProperty("--mouse-y", `${y}px`);
+    console.log("handleOnMouseHover");
 
 }
 
