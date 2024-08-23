6a28d10f8eed5548218dbdb3b42257983866275f
gstill498463 <gabriel.m.still@gmail.com>
2024-08-23 15:39:24 -0700

    updated projects and general layout

--- PortfolioDevHost/obj/Debug/net8.0/PortfolioDevHost.AssemblyInfo.cs
+++ PortfolioDevHost/obj/Debug/net8.0/PortfolioDevHost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioDevHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba35f7a86bf7da3b0c8e6fea316e30c3d6cf9737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ed04c30357b5931fbeb2d6cd80a186e5142a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioDevHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioDevHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
